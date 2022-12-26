b40a085923d148cb0580709c30f74909c6c14bdf
anikmishra836 <anikmishra836@gmail.com>
2022-12-26 18:34:20 +0530

    added .env file

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+// importing dotenv to make visible the process env varables in application.
+import dotenv from 'dotenv';
+
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -14,11 +17,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://anikmishra:anikmishra836@cluster0.5g8i3oz.mongodb.net/?retryWrites=true&w=majority'
+// const CONNECTION_URL = 'mongodb+srv://anikmishra:anikmishra836@cluster0.5g8i3oz.mongodb.net/?retryWrites=true&w=majority'
 
 
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(process.env.PORT || 5000, () => {
     console.log('Server is listening on port 5000');
 }))
