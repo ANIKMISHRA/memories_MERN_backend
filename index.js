2345d0a5da34adb2bdbc271f1c2357cd995b0c5b
anikmishra836 <anikmishra836@gmail.com>
2022-12-27 18:46:09 +0530

    worked on authentication

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 dotenv.config();
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 // const CONNECTION_URL = 'mongodb+srv://anikmishra:anikmishra836@cluster0.5g8i3oz.mongodb.net/?retryWrites=true&w=majority'
 
