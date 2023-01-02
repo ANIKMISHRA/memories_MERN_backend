1750e3318496e0f0f1115f0fcf177611f6e46efc
anikmishra836 <anikmishra836@gmail.com>
2023-01-02 18:44:08 +0530

    implement pagination and search component

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,13 +6,35 @@ import PostMessage from "../models/postMessage.js";
 const router = express.Router();
 
 export const getPosts = async (req, res) => {
-   try {
-    const postMessages = await PostMessage.find();
+    const { page } = req.query;
 
-    res.status(200).json(postMessages);
+   try {
+     
+    const LIMIT = 3;
+    const startIndex = (Number(page) - 1) * LIMIT;
+    const total = await PostMessage.countDocuments({});
+
+    const posts = await PostMessage.find().sort({_id: -1}).limit(LIMIT).skip(startIndex);
+
+    res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
    } catch (error) {
     res.status(404).json({ message: error.message })
    }  
+}
+
+export const getPostsBySearch = async (req, res) => {
+
+    const { searchQuery, tags } = req.query;
+
+    try {
+        const title = new RegExp(searchQuery, 'i');
+
+        const posts = await PostMessage.find({ $or: [{ title }, { tags: { $in: tags.split(',')}} ]});
+
+        res.json({ data: posts });
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 }
 
 export const createPost = async (req, res) => {
@@ -54,7 +76,7 @@ export const likePost = async (req, res) => {
 
     if(!req.userId) return res.json({ message: "Unauthenticated" });
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No like with that id');
 
     const post = await PostMessage.findById(id);
 
