1b7e3dea852fa26333e94a5be031203b7fe8ae1c
anikmishra836 <anikmishra836@gmail.com>
2023-01-04 18:46:53 +0530

    worked on user profile functonality

--- controllers/user.js
+++ controllers/user.js
@@ -47,3 +47,14 @@ export const signup = async (req, res) => {
     }
     
 }
+
+export const getUser = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findById(id);
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong"});
+    }
+}
